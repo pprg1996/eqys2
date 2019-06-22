5f2062a8285f421bc849af4dbaa9e6016423d5ee
pprg1996 <pprg1996@gmail.com>
2019-06-22 17:41:51 -0400

    Responsive - Header

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const header = document.querySelector("header");
-addEventListener("scroll", () => {
+const onScroll = () => {
   if (pageYOffset > 0) header.classList.add("header-show");
   else header.classList.remove("header-show");
+};
+
+const header = document.querySelector("header");
+addEventListener("scroll", onScroll);
+// addEventListener("touchmove", onScroll);
+
+const burger = document.querySelector(".burger-link");
+const menu = document.querySelector(".menu");
+const cerrar = document.querySelector(".cerrar-link");
+
+burger.addEventListener("click", event => {
+  event.preventDefault();
+
+  menu.classList.add("show-menu");
+});
+
+cerrar.addEventListener("click", event => {
+  event.preventDefault();
+
+  menu.classList.remove("show-menu");
 });
